b243a7471c75d7e15a1d824576c17624ef55b592
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-03-19 15:12:01 +0100

    Better doc for enter/exit
    Add open_state/close_state

--- R/tween_state.R
+++ R/tween_state.R
@@ -5,7 +5,10 @@
 #' This setup lets you take more control over each state change and allows you
 #' to work with datasets with uneven number of rows, flexibly specifying what
 #' should happen with entering and exiting data. `keep_state()` is a simpel
-#' helper for letting you pause at a state.
+#' helper for letting you pause at a state. `open_state()` is a shortcut from
+#' tweening from an empty dataset with a given `enter()` function while
+#' `close_state()` is the same but will instead tween into an empty dataset with
+#' a given `exit()` function.
 #'
 #' @param .data A data.frame to start from. If `.data` is the result of a prior
 #' tween, only the last frame will be used for the tween. The new tween will
@@ -20,17 +23,54 @@
 #' @param nframes The number of frames to calculate for the tween
 #'
 #' @param id The column to match observations on. If `NULL` observations will be
-#' matched by position
+#' matched by position. See the *Match, Enter, and Exit* section for more
+#' information.
 #'
 #' @param enter,exit functions that calculate a start state for new observations
 #' that appear in `to` or an end state for observations that are not present in
 #' `to`. If `NULL` the new/old observations will not be part of the tween. The
 #' function gets a data.frame with either the start state of the exiting
 #' observations, or the end state of the entering observations and must return
-#' a modified version of that data.frame.
+#' a modified version of that data.frame. See the *Match, Enter, and Exit*
+#' section for more information.
 #'
 #' @return A data.frame containing all the intermediary states in the tween,
 #' each state will be enumerated by the `.frame` column
+#'
+#' @section Match, Enter, and Exit:
+#' When there are discrepancies between the two states to tweeen between you
+#' need a way to resolve the discrepancy before calculating the intermediary
+#' states. With discrepancies we mean that some data points are present in the
+#' start state and not in the end state, and/or some are present in the end
+#' state but not in the start state. A simple example is that the start state
+#' contains 100 rows and the end state contains 70. There are 30 missing rows
+#' that we need to do something about before we can calculate the tween.
+#'
+#' **Making pairs**
+#' The first question to answer is "How do we know which observations are
+#' disappearing (*exiting*) and/or appearing (*entering*)?". This is done with
+#' the `id` argument which should give a column name to match rows between the
+#' two states on. If `id = NULL` the rows will be matched by position (in the
+#' above example the last 30 rows in the start state will be entering). The `id`
+#' column must only contain unique values in order to work.
+#'
+#' **Making up states**
+#' Once the rows in each state has been paired you'll end up with three sets of
+#' data. One containing rows that is present in both the start and end state,
+#' one containing rows only present in the start state, and one only containing
+#' rows present in the end state. The first group is easy - here you just tween
+#' between each rows - but for the other two we'll need some state to start or
+#' end the tween with. This is really the purpose of the `enter` and `exit`
+#' functions. They take a data frame containing the subset of data that has not
+#' been matched and must return a new data frame giving the state that these
+#' rows must be tweened from/into. A simple example could be an `enter` function
+#' that sets the variable giving the opacity in the plot to 0 - this will make
+#' the new points fade into view during the transition.
+#'
+#' **Ignoring discrepancies**
+#' The default values for `enter` and `exit` is `NULL`. This value indicate that
+#' non-matching rows should simply be ignored for the transition and simply
+#' appear in the last frame of the tween. This is the default.
 #'
 #' @export
 #'
@@ -49,6 +89,28 @@
 #'   tween_state(data2, 'linear', 50) %>%
 #'   keep_state(20) %>%
 #'   tween_state(data1, 'bounce-out', 50)
+#'
+#' # Using enter and exit (made up numbers)
+#' df1 <- data.frame(
+#'   country = c('Denmark', 'Sweden', 'Norway'),
+#'   population = c(5e6, 10e6, 3.5e6)
+#' )
+#' df2 <- data.frame(
+#'   country = c('Denmark', 'Sweden', 'Norway', 'Finland'),
+#'   population = c(6e6, 10.5e6, 4e6, 3e6)
+#' )
+#' df3 <- data.frame(
+#'   country = c('Denmark', 'Norway'),
+#'   population = c(10e6, 6e6)
+#' )
+#' to_zero <- function(x) {
+#'   x$population <- 0
+#'   x
+#' }
+#' pop_devel <- df1 %>%
+#'   tween_state(df2, 'cubic-in-out', 50, id = 'country', enter = to_zero) %>%
+#'   tween_state(df3, 'cubic-in-out', 50, id = 'country', enter = to_zero,
+#'               exit = to_zero)
 #'
 tween_state <- function(.data, to, ease, nframes, id = NULL, enter = NULL, exit = NULL) {
     from <- get_last_frame(.data)
@@ -98,10 +160,34 @@ keep_state <- function(.data, nframes) {
     states$.frame <- rep(seq_len(nframes), each = nrow(state))
     with_prior_frames(.data, states)
 }
-
+#' @rdname tween_state
+#' @export
+open_state <- function(.data, ease, nframes, enter) {
+    to <- get_first_frame(.data)
+    if (nrow(to) != nrow(.data)) nframes <- nframes + 1
+    from <- enter(to)
+    tweendata <- tween_state(from, to, ease, nframes)
+    with_later_frames(.data, tweendata)
+}
+#' @rdname tween_state
+#' @export
+close_state <- function(.data, ease, nframes, exit) {
+    from <- get_last_frame(.data)
+    if (nrow(from) != nrow(.data)) nframes <- nframes + 1
+    to <- exit(from)
+    tweendata <- tween_state(from, to, ease, nframes)
+    with_prior_frames(.data, tweendata)
+}
 get_last_frame <- function(data) {
     if ('.frame' %in% names(data)) {
         data[data$.frame == max(data$.frame), names(data) != '.frame']
+    } else {
+        data
+    }
+}
+get_first_frame <- function(data) {
+    if ('.frame' %in% names(data)) {
+        data[data$.frame == 1, names(data) != '.frame']
     } else {
         data
     }
@@ -111,6 +197,15 @@ with_prior_frames <- function(prior, new_tween) {
         prior <- prior[prior$.frame != max(prior$.frame), , drop = FALSE]
         new_tween$.frame <- new_tween$.frame + max(prior$.frame)
         rbind(prior, new_tween)
+    } else {
+        new_tween
+    }
+}
+with_later_frames <- function(later, new_tween) {
+    if ('.frame' %in% names(later)) {
+        later <- later[later$.frame != 1, , drop = FALSE]
+        later$.frame <- later$.frame + max(new_tween$.frame)
+        rbind(new_tween, later)
     } else {
         new_tween
     }
@@ -129,7 +224,7 @@ complete_states <- function(from, to, id, enter, exit) {
         exiting <- !from_id %in% to_id
         exits <- from[entering, , drop = FALSE]
 
-        if (is.null(enter)) {
+        if (is.null(enter) || sum(entering) == 0) {
             to <- to[!entering, , drop = FALSE]
             to_id <- to_id[!entering]
             enters <- to[0, , drop = FALSE]
@@ -139,7 +234,7 @@ complete_states <- function(from, to, id, enter, exit) {
             enters <- enter(to[entering, , drop = FALSE])
             enter_id <- to_id[entering]
         }
-        if (is.null(exit)) {
+        if (is.null(exit) || sum(exiting) == 0) {
             from <- from[!exiting, , drop = FALSE]
             from_id <- from_id[!entering]
             exits <- from[0, , drop = FALSE]
