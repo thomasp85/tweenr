0275ac7b0c026fdcce5e2273dc7ad2ad26ecfbf2
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-04-16 14:49:52 +0200

    Stupid fix to stupid copy paste

--- R/tween_state.R
+++ R/tween_state.R
@@ -140,7 +140,7 @@ tween_state <- function(.data, to, ease, nframes, id = NULL, enter = NULL, exit 
             datetime = interpolate_datetime_state(d, state),
             constant = interpolate_constant_state(d, state),
             numlist =,
-            list = interpolate_list_state(d, states)
+            list = interpolate_list_state(d, state)
         )
     })
     tweendata <- as.data.frame(tweendata)
