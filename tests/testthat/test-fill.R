48ea2fda34efa638f90026831e5f90145fc08158
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-09-26 13:15:36 +0200

    test tween_fill

---
+++ tests/testthat/test-fill.R
@@ -0,0 +1,19 @@
+context("fill")
+
+df <- data.frame(
+  x = c(1, NA, NA, NA, 6, 4, NA, NA, NA, NA, 20),
+  type = c('a', NA, NA, NA, 'b', 'c', NA, NA, NA, NA, 'd'),
+  col = c('red', NA, NA, NA, 'blue', 'green', NA, NA, NA, NA, 'black'),
+  stringsAsFactors = FALSE
+)
+test_that("tween_fill works", {
+  tween <- tween_fill(df, 'linear')
+  expect_equal(dim(df), dim(tween))
+  expect_equal(tween$x[3], 3.5)
+  expect_equal(tween$col[10], '#110010FF')
+
+  expect_equal(tween_fill(df$col, 'linear'), tween$col)
+
+  tween <- tween_fill(df[-c(1, 11), 1], 'linear')
+  expect_equal(tween, df$x[-c(1, 11)])
+})
