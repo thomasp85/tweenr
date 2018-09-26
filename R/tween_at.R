a40c044b8a524134b5fc65d88d7f7991d41a6c3c
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-09-26 13:15:26 +0200

    check names of columns

--- R/tween_at.R
+++ R/tween_at.R
@@ -38,6 +38,7 @@ tween_at <- function(from, to, at, ease) {
   if (nrow(from) != nrow(to)) {
     stop('from and to must be same length', call. = FALSE)
   }
+  stopifnot(names(from) == names(to))
   at <- rep(at, length.out = nrow(from))
   ease <- rep(ease, length.out = ncol(from))
   classes <- col_classes(from)
